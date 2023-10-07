30a14558205e07c36e6fb86de54819243bad2044
rafimd422 <rafimf2222@gmail.com>
2023-10-08 01:59:07 +0600

    Loaded data from service.json file by using fetch api and  useEffect

--- src/Components/Pages/Home/Service.jsx
+++ src/Components/Pages/Home/Service.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Service = () => {
-  return (
-    <div className='h-screen'>
-      SerVice
-    </div>
-  )
-}
+  const [service, setService] = useState([]);
 
-export default Service
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
+
+  return (
+    <div className="h-fit my-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-16 mx-auto shadow-lg container">
+        {service.map((service) => (
+          <div
+            className="max-w-sm rounded overflow-hidden shadow-xl border-gray-600 bg-[#080d1d]"
+            key={service.id}
+          >
+            <img className="w-full" src={service.img} alt={service.title} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{service.title}</div>
+              <p className="text-gray-400 text-base">
+                {service.description.slice(0, 130)}{" "}
+                <Link className="text-blue-700 font-bold" to={"serviceDetails"}>
+                  Read more...
+                </Link>
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              {service.hashtags.map((hash) => (
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  key={hash}
+                >
+                  {hash}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Service;
