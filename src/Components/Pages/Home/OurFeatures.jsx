2e11af5e14d81f1f1761acd9fa4192abc1e6a512
rafimd422 <rafimf2222@gmail.com>
2023-10-08 17:05:37 +0600

    complated FAQ section by using and modify a component from tailgrid

--- src/Components/Pages/Home/OurFeatures.jsx
+++ src/Components/Pages/Home/OurFeatures.jsx
@@ -42,7 +42,7 @@ const OurFeatures = () => {
   return (
 <section className="m-4 md:m-8 text-gray-100">
 	<div className="container mx-auto p-4 my-6 space-y-2 text-center">
-		<h2 className="md:text-5xl text-4xl mb-4 font-bold">Built to empower every team</h2>
+		<h2 className="md:text-5xl text-4xl mb-4 font-bold text-primary">Built to empower every team</h2>
 		<p className="text-gray-400 text-3xl font-bold border-b-2 border-red-600 p-3 w-fit mx-auto">Our Features</p>
 	</div>
 	<div className="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
